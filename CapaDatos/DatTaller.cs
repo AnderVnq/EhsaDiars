3871fbff212bfdb21790f4cd2fb553ed41f005e6
Ander19 <ndrsnvenega@gmail.com>
2024-05-22 01:02:36 -0500

    cada datos de taller

--- CapaDatos/DatTaller.cs
+++ CapaDatos/DatTaller.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CapaEntidad;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,89 @@ namespace CapaDatos
 {
     public class DatTaller
     {
+
+
+        #region singleton
+
+        private static readonly DatTaller _instace = new DatTaller();
+        public static DatTaller Instance
+        {
+            get { return _instace; }
+        }
+        #endregion
+
+
+
+        public List<Taller> listar_taller()
+        {
+            SqlCommand sqlCommand = null;
+            List<Taller> lista = new List<Taller>();
+            try
+            {
+                SqlConnection cn = Conexion.Instance.Conectar();
+                sqlCommand = new SqlCommand("splistarTaller", cn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    Taller taller = new Taller();
+                    taller.entMantenimiento = new entMantenimiento();
+                    taller.id = Convert.ToInt32(reader["id_taller"]);
+                    taller.entMantenimiento.id = Convert.ToInt32(reader["id_mantenimiento"]);
+                    taller.nombre = reader["nombre"].ToString();
+                    taller.direccion = reader["direccion"].ToString();
+                    taller.telefono = reader["telefono"].ToString();
+                    lista.Add(taller);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lista;
+
+        }
+
+        public List<Mantenimiento_interno> listar_Mante_interno()
+        {
+            SqlCommand sqlCommand = null;
+            List<Mantenimiento_interno> lista = new List<Mantenimiento_interno>();
+            try
+            {
+                SqlConnection cn = Conexion.Instance.Conectar();
+                sqlCommand = new SqlCommand("listaManteInterno", cn);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                cn.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+
+                    Mantenimiento_interno taller = new Mantenimiento_interno();
+                    taller.id = Convert.ToInt32(reader["id_taller"]);
+                    taller.id_mantenimiento = Convert.ToInt32(reader["id_mantenimiento"]);
+                    taller.tipo_matenimiento = reader["tipo_mantenimiento"].ToString();
+                    taller.fecha_mantenimiento = Convert.ToDateTime(reader["fecha_mantenimiento"]);
+                    taller.costo = Convert.ToInt32(reader["costo"]);
+                    taller.nombre = reader["nombre"].ToString();
+                    lista.Add(taller);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return lista;
+        }
+
+
+
     }
 }
