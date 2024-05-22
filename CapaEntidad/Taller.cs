21a5c70da118ce87c15c7dfd936d8d8d5b9a5c5f
Ander19 <ndrsnvenega@gmail.com>
2024-05-22 01:02:51 -0500

    entidad taller

--- CapaEntidad/Taller.cs
+++ CapaEntidad/Taller.cs
@@ -8,6 +8,24 @@ namespace CapaEntidad
 {
     public class Taller
     {
+        public int id {  get; set; }
+        public entMantenimiento entMantenimiento { get; set; }
+        public string nombre { get; set; }
+        public string direccion {  get; set; }
+        public string telefono { get; set; }
 
     }
+
+    public class Mantenimiento_interno
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int id_mantenimiento { get; set; }
+        public string tipo_matenimiento { get; set; }
+        public DateTime fecha_mantenimiento { get; set; }
+        public float costo { get; set; }
+    }
+
+
+
 }
