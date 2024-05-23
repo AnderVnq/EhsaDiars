545f94b7360071810591affbc867f93a9fdedcb2
Ander19 <ndrsnvenega@gmail.com>
2024-05-22 22:32:47 -0500

    fix nombre de la entidad y se añadio atributos a la orden

---
+++ CapaEntidad/Contratistas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Contratistas
+    {
+
+        public int id { get; set; }
+        public string tipo_documento { get; set; }
+        public string nro_Documento { get; set; }
+        public string razon_Social { get; set; }
+        public string direccion { get; set; }
+        public string nro_Contacto { get; set; }
+        public string correo { get; set; }
+        public string especialidad { get; set; }
+    }
+}
--- CapaEntidad/OrdenMantenimiento.cs
+++ CapaEntidad/OrdenMantenimiento.cs
@@ -8,5 +8,18 @@ namespace CapaEntidad
 {
     public class OrdenMantenimiento
     {
+
+        public int id {  get; set; }
+        public int id_contratista { get; set; }
+        public string razon_social { get; set; }
+        public int id_mantenimiento { get; set; }
+        public string tipo_mantenimiento { get; set; }
+        public float costo {  get; set; }
+        public string componente { get; set; }
+        public DateTime fecha_inicio { get; set; }
+        public DateTime fecha_fin {  get; set; }
+        public string estado { get; set; }
+
+
     }
 }
