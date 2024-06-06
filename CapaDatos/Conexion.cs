5e22054efa329f551937cd9a42e614531ced2a38
Ander19 <ndrsnvenega@gmail.com>
2024-06-06 00:04:21 -0500

    cambio de tipo de conexion a mysql

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -9,14 +10,20 @@ namespace CapaDatos
 {
     public class Conexion
     {
+        // Instancia singleton
         private static readonly Conexion _instance = new Conexion();
-        public static Conexion Instance { get {  return _instance; } }
-        public SqlConnection Conectar()
-        {
-            SqlConnection conexion = new SqlConnection();
-            conexion.ConnectionString= "Data Source=ANDER\\SQLSERVER; Initial Catalog = ETHSA_bd2;" +"Integrated Security=true";
-            return conexion;
+        public static Conexion Instance { get { return _instance; } }
 
+        //private string connectionString = "Server=bd9tpws8jfqulufuy2ur-mysql.services.clever-cloud.com;Port=3306;Database=bd9tpws8jfqulufuy2ur;Uid=u7mam79auuacon4t;Pwd=NV0qEqUJCdvE7bREVvAV;";
+        private string connectionString = "Server=localhost;Port=3306;Database=ethsa_db2;Uid=root;Pwd=heaveny2;";
+        // Constructor privado para evitar la instanciación desde fuera
+        private Conexion() { }
+
+        // Método para obtener la conexión MySQL
+        public MySqlConnection Conectar()
+        {
+            MySqlConnection conexion = new MySqlConnection(connectionString);
+            return conexion;
         }
     }
 }
