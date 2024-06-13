b2c042e2384b73978c644d75d454c9f47edd8851
Ander19 <ndrsnvenega@gmail.com>
2024-06-13 01:03:59 -0500

    se agrego la entidad pagos

---
+++ CapaEntidad/Pagos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Pagos
+    {
+
+        public int id { get; set; }
+        public int id_orden { get; set; }
+        public int id_tipo_pago { get; set; }
+        public string nombre_e { get; set; }
+        public float monto_total { get; set; }
+        public DateTime fecha_pago { get; set; }
+        public string concepto { get; set; }
+        public string nro_cuenta { get; set; }
+
+    }
+}
