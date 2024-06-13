1fadfa982b7bf622577034aea04c11f618581ab9
Ander19 <ndrsnvenega@gmail.com>
2024-06-13 01:05:15 -0500

    form de dashboard

---
+++ CapaPresentacion/Usuarios/FormDashboard.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CapaPresentacion.Usuarios
+{
+    partial class FormDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDashboard));
+            label1 = new Label();
+            panel1 = new Panel();
+            label5 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            panel2 = new Panel();
+            label6 = new Label();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            panel3 = new Panel();
+            label7 = new Label();
+            label4 = new Label();
+            mySqlCommand1 = new MySql.Data.MySqlClient.MySqlCommand();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Historic", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(147, 30);
+            label1.TabIndex = 0;
+            label1.Text = "DASHBOARD";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(42, 53);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(270, 109);
+            panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(164, 66);
+            label5.Name = "label5";
+            label5.Size = new Size(28, 21);
+            label5.TabIndex = 2;
+            label5.Text = "20";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(20, 51);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(84, 48);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(72, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 21);
+            label2.TabIndex = 0;
+            label2.Text = "Conductores";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(label3);
+            panel2.Location = new Point(378, 53);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(285, 109);
+            panel2.TabIndex = 2;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(174, 66);
+            label6.Name = "label6";
+            label6.Size = new Size(28, 21);
+            label6.TabIndex = 3;
+            label6.Text = "30";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(18, 49);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 50);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(79, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(136, 21);
+            label3.TabIndex = 1;
+            label3.Text = "Mantenimientos";
+            label3.Click += label3_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label7);
+            panel3.Controls.Add(label4);
+            panel3.Location = new Point(696, 53);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(270, 109);
+            panel3.TabIndex = 2;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(81, 66);
+            label7.Name = "label7";
+            label7.Size = new Size(28, 21);
+            label7.TabIndex = 3;
+            label7.Text = "40";
+            label7.Click += label7_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(81, 10);
+            label4.Name = "label4";
+            label4.Size = new Size(84, 21);
+            label4.TabIndex = 2;
+            label4.Text = "Vehiculos";
+            label4.Click += label4_Click;
+            // 
+            // mySqlCommand1
+            // 
+            mySqlCommand1.CacheAge = 0;
+            mySqlCommand1.Connection = null;
+            mySqlCommand1.EnableCaching = false;
+            mySqlCommand1.Transaction = null;
+            // 
+            // FormDashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1044, 561);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "FormDashboard";
+            Text = "FormDashboard";
+            Load += FormDashboard_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private Panel panel2;
+        private Label label3;
+        private Panel panel3;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Label label5;
+        private Label label6;
+        private PictureBox pictureBox2;
+        private Label label7;
+        private MySql.Data.MySqlClient.MySqlCommand mySqlCommand1;
+    }
+}
